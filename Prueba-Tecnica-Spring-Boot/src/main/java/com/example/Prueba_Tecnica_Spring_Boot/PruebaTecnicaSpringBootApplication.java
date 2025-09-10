33d1976b021123de82ccc218efd27bb6666359c7
AlbertoEscamilla <alesdo2003@gmail.com>
2025-09-10 21:04:45 +0200

    proyecto inicial

---
+++ Prueba-Tecnica-Spring-Boot/src/main/java/com/example/Prueba_Tecnica_Spring_Boot/PruebaTecnicaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.Prueba_Tecnica_Spring_Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaSpringBootApplication.class, args);
+	}
+
+}
---
+++ Prueba-Tecnica-Spring-Boot/src/test/java/com/example/Prueba_Tecnica_Spring_Boot/PruebaTecnicaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Prueba_Tecnica_Spring_Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
